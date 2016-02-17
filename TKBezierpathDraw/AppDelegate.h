76c808aa5165693275c62e2cfb828b884e3ef5a2
xiaoxiaozai <foreverlosda@vip.qq.com>
2016-02-17 15:21:27 +0800

    create

---
+++ TKBezierpathDraw/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TKBezierpathDraw
+//
+//  Created by 翟宇 on 16/2/17.
+//  Copyright © 2016年 翟宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TKBezierpathDraw/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TKBezierpathDraw
+//
+//  Created by 翟宇 on 16/2/17.
+//  Copyright © 2016年 翟宇. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
