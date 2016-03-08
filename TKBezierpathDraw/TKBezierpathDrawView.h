6f91f2ae5f31798ba8a3e39cc06b612c19a2b826
xiaoxiaozai <foreverlosda@vip.qq.com>
2016-03-08 14:14:14 +0800

    UPDATE

---
+++ TKBezierpathDraw/TKBezierpathDrawView.h
@@ -0,0 +1,16 @@
+//
+//  TKBezierpathDrawView.h
+//  TKBezierpathDraw
+//
+//  Created by 翟宇 on 16/2/17.
+//  Copyright © 2016年 翟宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TKBezierpathDrawView : UIView
+
+@property(nonatomic,strong)NSString*message;
+@property (nonatomic,assign)CGFloat progress;
+@property (nonatomic,strong)UIColor* colorTemp;
+@end
